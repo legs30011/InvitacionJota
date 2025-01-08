f3ea692fb91f7246bc1c30ee095a821bf3b0a1a2
LuisGamboaS <legs30011@gmail.com>
2025-01-08 14:49:46 -0300

    invitacion

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express();
+const path = require('path')
+
+app.use(express.static(path.join(__dirname,'public')));
+app.use("/media", express.static('media'));
+app.use("/public", express.static('public'));
+
+
+app.get('/', (req,res) =>{
+    res.sendFile(path.join(__dirname, 'public/html/index.html'))
+})
+
+app.listen(1234);
