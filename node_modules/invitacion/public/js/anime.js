b735870016f0c715b6916237965baed55adc4328
LuisGamboaS <legs30011@gmail.com>
2025-01-08 15:34:23 -0300

    jotaboda

---
+++ node_modules/invitacion/public/js/anime.js
@@ -0,0 +1,15 @@
+
+
+// window.addEventListener("scroll", (event) => {
+//     let scroll = this.scrollY;
+//     console.log(scroll)
+//     if(scroll >=0 && scroll < 308){
+//         anime({
+//             targets: '.information-section__verse__content',
+//             translateX:[
+//                 {value: 50, duration: 3000, delay:500,easing: 'easeInOutQuad'},
+//             ],
+//             suspendWhenDocumentHidden: true
+//         })
+//     }
+// });
